972fbf604dfe5c6c806f282869988edd326b5a6b
Subigya Lamichhane <subigyalamichhane@gmail.com>
2023-11-14 14:51:40 +0000

    set up storybooks

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import "../../global.css";
 
 interface ButtonProps {
   label: string;
+  // size is optional and defaults to "medium"
+  size?: "small" | "medium" | "large" | undefined;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button className="bg-black">{props.label}</button>;
+  const size = props.size || "medium";
+
+  return (
+    <button
+      className={`bg-accents-9
+    ${size === "small" ? "text-sm py-1 px-2" : ""}
+    ${size === "medium" ? "text-base py-2 px-4" : ""}
+    ${size === "large" ? "text-lg py-3 px-6" : ""}
+  `}
+    >
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
